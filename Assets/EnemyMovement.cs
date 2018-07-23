b7466b4f9963c7edcb6f347413805e657046f73b
Julian McNeill <julesdowork@gmail.com>
2018-07-23 15:41:28 -0400

    C-Sharp Lists For Custom Path

---
+++ Assets/EnemyMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    [SerializeField] List<Block> path;
+
+	// Use this for initialization
+	void Start ()
+    {
+		foreach (Block block in path)
+        {
+            print(block.gameObject.name);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
