36d3c9b269eb033d17942cdc930c0136a3ba90e6
Julian McNeill <julesdowork@gmail.com>
2018-07-23 17:36:55 -0400

    Instance Variables And Constants

--- Assets/Waypoint.cs
+++ Assets/Waypoint.cs
@@ -2,15 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Waypoint : MonoBehaviour {
+public class Waypoint : MonoBehaviour
+{ 
+    const int gridSize = 10;
+    Vector2Int gridPos;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public int GetGridSize()
+    {
+        return gridSize;
+    }
+
+    public Vector2 GetGridPos()
+    {
+        return new Vector2(
+            Mathf.RoundToInt(transform.position.x / gridSize) * gridSize,
+            Mathf.RoundToInt(transform.position.z / gridSize) * gridSize
+        );
+    }
 }
