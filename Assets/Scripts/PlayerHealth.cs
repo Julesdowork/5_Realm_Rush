aa2110d3f8a661b67183121814dc42787e56b635
Julian McNeill <julesdowork@gmail.com>
2018-07-25 17:31:00 -0400

    Health For Base

---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    [SerializeField] int health = 20;
+
+    void OnTriggerEnter(Collider other)
+    {
+        print(other.name);
+        health--;
+    }
+}
